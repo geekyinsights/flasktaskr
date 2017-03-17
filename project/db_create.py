7cebb42ddacbd4b571ee035b1b1a3c5bdfce6ad5
Christina Bernard <cbernard2010@gmail.com>
2017-03-17 15:01:40 -0500

    Conversions from SQL to SQL Alchemy

--- project/db_create.py
+++ project/db_create.py
@@ -1,19 +1,15 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    #create cursor object used to execute SQL commands
-    c = connection.cursor()
 
-    #create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status  INTEGER
-                 NOT NULL)""")
-    #insert dummy data into the TABLE
-    c.execute(
-                'INSERT INTO tasks (name, due_date, priority, status)'
-                ' VALUES ("Finish this tutorial", "3/25/2015", 10, 1)')
+#creae the database and the db TABLE
+db.create_all()
 
-    c.execute(
-                'INSERT INTO tasks (name, due_date, priority, status)'
-                ' VALUES ("Finish Real Python Cource 2", "3/25/2015", 10, 1)')
+#insert database
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
+
+#commit the changes
+db.session.commit()
